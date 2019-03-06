ef26b7679d38db1bfe24a73efd09e0916e10f9c3
hk <16028272223@qq.com>
2019-03-06 10:48:27 +0800

    二分查找

--- src/main/java/com/cyg/suanfa/algorithm/TwoPointsSearch.java
+++ src/main/java/com/cyg/suanfa/algorithm/TwoPointsSearch.java
@@ -30,7 +30,7 @@ public class TwoPointsSearch {
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
         TwoPointsSearch twoPointsSearch = new TwoPointsSearch();
         System.out.println(twoPointsSearch.biSearch(array, 10));
     }
