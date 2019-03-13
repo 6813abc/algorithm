26559a07e5b94ae5c7eef75a68a155de58b04239
hk <16028272223@qq.com>
2019-03-13 21:38:28 +0800

    从上往下打印出二叉树的每个节点，同层节点从左至右打印

---
+++ src/main/java/com/cyg/suanfa/algorithm/PrintFromTopToBottom.java
@@ -0,0 +1,56 @@
+package com.cyg.suanfa.algorithm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印:
+ * 利用队列的先进先出的特性
+ * @author cyg
+ * @date 2019/3/13 下午8:45
+ **/
+public class PrintFromTopToBottom {
+    static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode treeNode = queue.poll();
+            if (treeNode.left != null) {
+                queue.offer(treeNode.left);
+            }
+            if (treeNode.right != null) {
+                queue.offer(treeNode.right);
+            }
+            list.add(treeNode.val);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        PrintFromTopToBottom printFromTopToBottom = new PrintFromTopToBottom();
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(3);
+        TreeNode treeNode3 = new TreeNode(4);
+        treeNode.left = treeNode1;
+        treeNode.right = treeNode2;
+        treeNode1.left = treeNode3;
+        System.out.println(printFromTopToBottom.printFromTopToBottom(treeNode));
+    }
+}
+
