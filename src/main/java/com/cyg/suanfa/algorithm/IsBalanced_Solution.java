c0427294f1868731527c059eab7e72299e9639e4
hk <16028272223@qq.com>
2019-03-31 10:29:11 +0800

    二叉树的深度

--- src/main/java/com/cyg/suanfa/algorithm/IsBalanced_Solution.java
+++ src/main/java/com/cyg/suanfa/algorithm/IsBalanced_Solution.java
@@ -13,6 +13,7 @@ public class IsBalanced_Solution {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
+
         public TreeNode(int val) {
             this.val = val;
         }
@@ -22,16 +23,21 @@ public class IsBalanced_Solution {
         if (root == null) {
             return true;
         }
-        int leftLength = 1;
-        int rightLength = 1;
-        leftLength += TreeDepth(root.left);
-        rightLength += TreeDepth(root.right);
+        //得到左子树最大深度
+        int leftLength = TreeDepth(root.left);
+        //得到右子树最大深度
+        int rightLength = TreeDepth(root.right);
+        //比较左右子树的深度判断平衡因子
         if (Math.abs(leftLength - rightLength) > 1) {
             return false;
         }
+        //递归判断左右子树
+        IsBalanced_Solution(root.left);
+        IsBalanced_Solution(root.right);
         return true;
     }
 
+    //得到某棵树最大的最大深度
     public int TreeDepth(TreeNode root) {
         if (root == null) {
             return 0;
